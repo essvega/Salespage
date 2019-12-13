25451070a6fffe010c5b11ce46f52335f77231c5
Ess Vega <34743796+mountainclimber09@users.noreply.github.com>
2019-12-13 10:06:02 -0500

    second commit

---
+++ public/assets/js/sales.js
@@ -0,0 +1,9 @@
+
+// Should ask if the user is logged in. If the user hasn't logged in any single thing the user touch will open the page Sign in
+
+var whishing = document.querySelectorAll(".wishlist-btn");
+whishing.document.addEventListener("click", wishlist());
+
+function wishlist(){
+    alert("nas");
+}
---
+++ sales2.js
@@ -0,0 +1,53 @@
+var express = require("express"),
+    app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+/* ------ Preloader ------- 
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("value-animated").style.display = "none";
+  document.getElementById("head-page").style.display = "block";
+}
+
+/* ------ Preloader End -------*/
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.get("/Sign-In", function(req, res){
+    res.render("signin");
+});
+
+
+//To Do:
+//1- If user hasn't Logged In cannot see cart from index page
+//2- 
+app.get("/cart", function(req, res){
+    res.render("cart");
+})
+app.get("/category", function(req, res){
+    res.render("category");
+})
+app.get("/checkout", function(req, res){
+    res.render("checkout");
+})
+app.get("/contact", function(req, res){
+    res.render("contact");
+})
+app.get("/product", function(req, res){
+    res.render("product");
+})
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Started");
+});
